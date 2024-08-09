a41009f644a1f96bbb69d5323d6557a3629b7e68
SuperSimon81 <thalen.simon@gmail.com>
2024-08-09 22:17:55 +0700

    rearranged functions

--- source/plugin_RAMP.m
+++ source/plugin_RAMP.m
@@ -38,63 +38,6 @@ switch fcn
 end
 end
 
-function calculate_flow()
-global DATA SET RAMP
-
-nom = RAMP.nom;
-nop = RAMP.nop;
-numrois = RAMP.numrois;
-
-a = SET(nom).TSize * 2;
-h = waitbar(0, 'Calculating flow.');
-set(h, 'visible', 'off');
-myadjust(h, DATA.GUI.Segment);
-set(h, 'visible', 'on');
-
-RAMP.Roi = SET(nom).Roi(1:2);
-
-counter = 0;
-
-for rloop = 1:numrois
-
-    for tloop = RAMP.Roi(rloop).T
-        counter = counter + 1;
-        waitbar(counter/a, h);
-
-        mask = logical(segment('createmask', RAMP.outsize, RAMP.Roi(rloop).Y(:,tloop), RAMP.Roi(rloop).X(:,tloop)));
-        temp = SET(nop).IM(:,:,tloop,RAMP.Roi(rloop).Z);
-
-        veldata = RAMP.Roi(rloop).Sign * (temp - 0.5) * 2 * SET(nop).VENC;
-
-        if ~isempty(SET(nop).Flow.PhaseCorr)
-            if SET(nop).Flow.PhaseCorrTimeResolved
-                correction = SET(nop).Flow.PhaseCorr(:,:,tloop,RAMP.Roi(rloop).Z);
-            else
-                correction = SET(nop).Flow.PhaseCorr(:,:,1,RAMP.Roi(rloop).Z);
-            end
-            veldata = RAMP.Roi(rloop).Sign * (temp - 0.5 - correction) * 2 * SET(nop).VENC;
-        end
-
-        RAMP.velocity_unmasked(:,:,tloop) = veldata;
-        veldata = veldata(mask);
-
-        if isempty(veldata) && not(warnedempty)
-            mywarning('Empty ROI. Please try again', DATA.GUI.Segment);
-            warnedempty = true;
-        else
-            if rloop == 1
-                RAMP.mitral_velocity(tloop, :) = temp(mask);
-            elseif rloop == 2
-                RAMP.tricuspid_velocity(tloop, :) = temp(mask);
-            end
-        end
-    end
-end
-close(h);
-
-%end
-end
-
 function ok = init(no)
 global RAMP DATA SET NO
 ok = false; %inform if unable to launch GUI
@@ -290,26 +233,222 @@ gui.l(2) = plot([RAMP.mit_inds(RAMP.mit_min_ind) RAMP.mit_inds(RAMP.mit_min_ind)
 gui.l(3) = plot([RAMP.tri_inds(RAMP.tri_max_ind) RAMP.tri_inds(RAMP.tri_max_ind)],[0,256],'Parent',gui.handles.mmd_axes,'color','b','LineWidth',2);
 gui.l(4) = plot([RAMP.tri_inds(RAMP.tri_min_ind) RAMP.tri_inds(RAMP.tri_min_ind)],[0,256],'Parent',gui.handles.mmd_axes,'color','b','LineWidth',2);
 
-set(gui.handles.mit_axes, 'ButtonDownFcn', @curve_callback);
-set(gui.handles.tri_axes, 'ButtonDownFcn', @curve_callback);
+set(gui.handles.mit_axes, 'ButtonDownFcn', @curve_click_callback);
+set(gui.handles.tri_axes, 'ButtonDownFcn', @curve_click_callback);
 
-%select_maxmin();
 update_table();
 RAMP.gui = gui;
 ok = true;
 end
 
+%Core functions
+function calculate_flow()
+global DATA SET RAMP
 
-function setup_gui(gui)
+nom = RAMP.nom;
+nop = RAMP.nop;
+numrois = RAMP.numrois;
 
+a = SET(nom).TSize * 2;
+h = waitbar(0, 'Calculating flow.');
+set(h, 'visible', 'off');
+myadjust(h, DATA.GUI.Segment);
+set(h, 'visible', 'on');
 
+RAMP.Roi = SET(nom).Roi(1:2);
 
+counter = 0;
+
+for rloop = 1:numrois
+
+    for tloop = RAMP.Roi(rloop).T
+        counter = counter + 1;
+        waitbar(counter/a, h);
+
+        mask = logical(segment('createmask', RAMP.outsize, RAMP.Roi(rloop).Y(:,tloop), RAMP.Roi(rloop).X(:,tloop)));
+        temp = SET(nop).IM(:,:,tloop,RAMP.Roi(rloop).Z);
+
+        veldata = RAMP.Roi(rloop).Sign * (temp - 0.5) * 2 * SET(nop).VENC;
+
+        if ~isempty(SET(nop).Flow.PhaseCorr)
+            if SET(nop).Flow.PhaseCorrTimeResolved
+                correction = SET(nop).Flow.PhaseCorr(:,:,tloop,RAMP.Roi(rloop).Z);
+            else
+                correction = SET(nop).Flow.PhaseCorr(:,:,1,RAMP.Roi(rloop).Z);
+            end
+            veldata = RAMP.Roi(rloop).Sign * (temp - 0.5 - correction) * 2 * SET(nop).VENC;
+        end
+
+        RAMP.velocity_unmasked(:,:,tloop) = veldata;
+        veldata = veldata(mask);
+
+        if isempty(veldata) && not(warnedempty)
+            mywarning('Empty ROI. Please try again', DATA.GUI.Segment);
+            warnedempty = true;
+        else
+            if rloop == 1
+                RAMP.mitral_velocity(tloop, :) = temp(mask);
+            elseif rloop == 2
+                RAMP.tricuspid_velocity(tloop, :) = temp(mask);
+            end
+        end
+    end
+end
+close(h);
+end
+
+function img = flow_histogram(velocities)
+% Construct a flow histogram or spectral plot where each voxel contributes one intensity value on the y-level of its velocity.
+
+img = zeros([256 size(velocities,1)]);
+for time_index=1:size(velocities,1)
+    for pixel_index=1:length(velocities(time_index,:))
+        % + 1 is needed here because matlab is awful
+        ind = (velocities(time_index,pixel_index))*255+1;
+        ind(ind<0)=0;
+        ind(ind>255)=255;
+        img(round(ind),time_index)=img(round(ind),time_index)+1;
+    end
+end
+end
+
+function img = flow_histogram_no_low(velocities)
+% Construct a flow histogram or spectral plot where each voxel contributes one intensity value on the y-level of its velocity.
+% Exclude -4 to 4 v velocites like in Doppler echo
+
+dist = 4;
+img = zeros([256 size(velocities,1)]);
+for time_index=1:size(velocities,1)
+    for pixel_index=1:length(velocities(time_index,:))
+        % + 1 is needed here because matlab is awful
+
+        ind = (velocities(time_index,pixel_index))*255+1;
+        ind(ind<0)=0;
+        ind(ind>255)=255;
+        if ind >128+dist || ind < 128-dist
+            img(round(ind),time_index)=img(round(ind),time_index)+1;
+        end
+
+    end
+    img(128,time_index)=255;
+end
 
 end
 
+function curve= max_percentile_curve(velocity,percentile)
+%constructs the vt curve from the of voxels with above or equal to the nth
+%percentile in the timeframe
+for i=1:size(velocity,1)
+    value = prctile(velocity(i,:),percentile);
 
+    all_values = velocity(i,:);
+    values_above_percentile = all_values(all_values>=value);
+    %length(values_above_percentile)
+    curve(i) = sum(values_above_percentile)/length(values_above_percentile)*255+1;
+
+end
+end
+
+function curve = maxk_curve(velocity,k)
+%constructs the vt curve from the k nr of voxels with the highest velocity
+%in the timeframe
+
+for i=1:size(velocity,1)
+    curve(i)=(sum(maxk(velocity(i,:),k))/k)*255+1;
+end
+end
+
+function [peaks,inds] = find_ewave_peaks(velocity,splits,offset)
+%This function is used to identify the peaks of the ewave.
+ewaves={};
+global RAMP
+gui = RAMP.gui;
+
+for i=1:length(velocity)
+    if velocity(i)>128
+        velocity(i)=velocity(i);
+    else
+        velocity(i)=128;
+    end
+end
+m = mean(velocity)+offset;
+
+
+for i=1:length(velocity)
+    if velocity(i)>m
+        velocity(i)=velocity(i);
+    else
+        velocity(i)=m;
+    end
+end
+for i=1:length(splits)-1
+    ewaves{i}=velocity(splits(i):splits(i+1));
+end
+
+
+
+
+for i=1:size(ewaves,2)
+    %for ii=1:size(ewaves{i})
+    t = splits(i);
+
+    %bias = linspace(1,0.9,length(ewaves{i}));
+    %test = ewaves{i}.*bias;
+
+    [pks,locs,w,h] = findpeaks(ewaves{i});
+
+    if(isempty(pks))
+        peaks(i)=-1
+        inds(i)=t-9;
+    else
+        [maxv,maxind] = max(w);
+        peak_e_ind = locs(maxind);%min(locs);
+        peaks(i) =255-ewaves{i}(peak_e_ind);
+        inds(i) = peak_e_ind+t-1;
+
+    end
+    %end
+end
+
+end
+
+function splits = calculate_negative_flow(velocity,min_peak_distance)
+%this function is used to determine the negative peaks of mitral flow,
+%which corresponds to aortic outflow during systole. This is used to
+%separate each heartbeat
+
+global DATA SET RAMP
+gui=RAMP.gui;
+
+
+neg_velocity = sum(mink(velocity,25,2),2)/25;
+neg_velocity = neg_velocity(neg_velocity<0.5);
+%Plot for debugging
+%gui.test = plot(255-neg_velocity*255,'color','red','LineWidth',1,'Parent',gui.handles.mit_axes,'PickableParts','none');
+B = 1/5*ones(5,1);
+neg_velocity = filter(B,1,neg_velocity);
+[peak,loc]=findpeaks(-neg_velocity,'MinPeakDistance',min_peak_distance);
+splits = loc-3;
+end
+
+function draw_mmode(x)
+global SET RAMP
+gui=RAMP.gui;
+nom = RAMP.nom;
+nop = RAMP.nop;
+
+%gui.phase = imagesc(squeeze(SET(nom).IM(:,x,:)),'Parent',gui.handles.mmd_axes);
+set(gui.mmd,'CData',squeeze(SET(nom).IM(:,x,:)));
+end
+
+function [max_val,min_val,max_ind,min_ind] = get_maxmin(peaks)
+[max_val,max_ind] = max(peaks);
+[min_val,min_ind] = min(peaks);
+end
+
+%Callback functions
 function splits_callback(h,~)
-global RAMP DATA
+global RAMP
 gui = RAMP.gui;
 
 set (gcf, 'WindowButtonMotionFcn', @mouseMove);
@@ -357,14 +496,121 @@ set(gcf,"WindowButtonUpFcn", @click)
         set(gui.mit_peak(RAMP.mit_min_ind),'MarkerSize',10);
         set(gui.tri_peak(RAMP.tri_max_ind),'MarkerSize',10);
         set(gui.tri_peak(RAMP.tri_min_ind),'MarkerSize',10);
-        delete(gui.l(:));
-        gui.l(1) = plot([RAMP.mit_inds(RAMP.mit_max_ind) RAMP.mit_inds(RAMP.mit_max_ind)],[0,256],'Parent',gui.handles.mmd_axes,'color','r','LineWidth',2);
-        gui.l(2) = plot([RAMP.mit_inds(RAMP.mit_min_ind) RAMP.mit_inds(RAMP.mit_min_ind)],[0,256],'Parent',gui.handles.mmd_axes,'color','r','LineWidth',2);
-        gui.l(3) = plot([RAMP.tri_inds(RAMP.tri_max_ind) RAMP.tri_inds(RAMP.tri_max_ind)],[0,256],'Parent',gui.handles.mmd_axes,'color','b','LineWidth',2);
-        gui.l(4) = plot([RAMP.tri_inds(RAMP.tri_min_ind) RAMP.tri_inds(RAMP.tri_min_ind)],[0,256],'Parent',gui.handles.mmd_axes,'color','b','LineWidth',2);
+        %delete(gui.l(:));
+        
+        %set(gui.l(1),'XData',[RAMP.mit_inds(RAMP.mit_max_ind) RAMP.mit_inds(RAMP.mit_max_ind)])
+        %gui.l(1) = plot([RAMP.mit_inds(RAMP.mit_max_ind) RAMP.mit_inds(RAMP.mit_max_ind)],[0,256],'Parent',gui.handles.mmd_axes,'color','r','LineWidth',2);
+        %gui.l(2) = plot([RAMP.mit_inds(RAMP.mit_min_ind) RAMP.mit_inds(RAMP.mit_min_ind)],[0,256],'Parent',gui.handles.mmd_axes,'color','r','LineWidth',2);
+        %gui.l(3) = plot([RAMP.tri_inds(RAMP.tri_max_ind) RAMP.tri_inds(RAMP.tri_max_ind)],[0,256],'Parent',gui.handles.mmd_axes,'color','b','LineWidth',2);
+        %gui.l(4) = plot([RAMP.tri_inds(RAMP.tri_min_ind) RAMP.tri_inds(RAMP.tri_min_ind)],[0,256],'Parent',gui.handles.mmd_axes,'color','b','LineWidth',2);
+        update_mmode_lines();
+        
+
+        
         update_table();
     end
 
+end
+
+function peak_callback(~,event)
+%To mnanually switch peaks if something went wrong
+global RAMP
+gui = RAMP.gui;
+button = event.Button;
+source = event.Source;
+ind = get(source,'UserData');
+xdata = get(source,'XData')
+ydata = get(source,'YData')
+switch button
+    case 1 %left click set max value
+        asdasd
+        text(gui.handles.mit_axes,xdata(1),ydata(1) - 10,0,num2str(-(RAMP.mit_peaks(ind)-128)/128));
+
+
+
+end
+
+end
+
+function curve_click_callback(gcbo,event)
+
+global DATA RAMP
+gui = RAMP.gui;
+x = round(event.IntersectionPoint(1,1));
+
+for i=1:length(RAMP.splits)
+    dist(i)= RAMP.splits(i) - x;
+
+end
+dist(dist<0)=nan;
+
+[val, ind] = min(dist);
+ind = ind -1;
+
+
+if x>RAMP.splits(1) && x<RAMP.splits(end)
+    if gcbo == gui.handles.mit_axes
+        RAMP.mit_inds(ind) = x;
+        RAMP.mit_peaks(ind) = 255-RAMP.mit_curve(x);
+        set(gui.mit_peak(ind),'XData',[x x]);
+        set(gui.mit_peak(ind),'YData',[RAMP.mit_peaks(ind) RAMP.mit_peaks(ind)]);
+    end
+
+    if gcbo == gui.handles.tri_axes
+        RAMP.tri_inds(ind) = x;
+        RAMP.tri_peaks(ind) = 255-RAMP.tri_curve(x);
+        set(gui.tri_peak(ind),'XData',[x x]);
+        set(gui.tri_peak(ind),'YData',[RAMP.tri_peaks(ind) RAMP.tri_peaks(ind)]);
+    end
+end
+
+[RAMP.mit_min_val,RAMP.mit_max_val,RAMP.mit_min_ind,RAMP.mit_max_ind]=get_maxmin(RAMP.mit_peaks);
+[RAMP.tri_min_val,RAMP.tri_max_val,RAMP.tri_min_ind,RAMP.tri_max_ind]=get_maxmin(RAMP.tri_peaks);
+
+set([gui.mit_peak(:); gui.tri_peak(:)], 'MarkerSize', 5);
+set([gui.mit_peak(RAMP.mit_max_ind), gui.mit_peak(RAMP.mit_min_ind), gui.tri_peak(RAMP.tri_max_ind), gui.tri_peak(RAMP.tri_min_ind)], 'MarkerSize', 10);
+
+update_mmode_lines();
+update_table();
+
+end
+
+function checkboxes_callback(varargin)
+global DATA RAMP
+gui = RAMP.gui;
+switch varargin{1}.Tag
+    case 'checkbox_kmax_line'
+        set([gui.mit_curve gui.tri_curve],'Visible',get(varargin{1},'Value'));
+
+    case 'checkbox_beat_seg'
+        set(gui.split_line(:),'Visible',get(varargin{1},'Value'));
+end
+
+end
+
+function mag_click_callback(gcbo,event)
+%Select what slice to use as mmode
+global DATA RAMP;
+gui = RAMP.gui;
+pos = get(get(gco,'Parent'),'CurrentPoint');
+h = gco;
+
+t = pos(1,1);
+draw_mmode(round(t));
+
+set(RAMP.overview_line,'Xdata',[t t]);
+%uistack(RAMP.overview_line,'top');
+end
+
+%Update functions that update the gui after callbacks have been triggered 
+function update_mmode_lines()
+global RAMP
+gui = RAMP.gui;
+
+set(gui.l(1),'XData',[RAMP.mit_inds(RAMP.mit_max_ind) RAMP.mit_inds(RAMP.mit_max_ind)]);
+set(gui.l(2),'XData',[RAMP.mit_inds(RAMP.mit_min_ind) RAMP.mit_inds(RAMP.mit_min_ind)]);
+set(gui.l(3),'XData',[RAMP.tri_inds(RAMP.tri_max_ind) RAMP.tri_inds(RAMP.tri_max_ind)]);
+set(gui.l(4),'XData',[RAMP.tri_inds(RAMP.tri_min_ind) RAMP.tri_inds(RAMP.tri_min_ind)]);
 end
 
 function update_curve()
@@ -475,249 +721,7 @@ end
 num2clip(gui.clip_array);
 end
 
-function peak_callback(~,event)
-%To mnanually switch peaks if something went wrong
-global RAMP
-gui = RAMP.gui;
-button = event.Button;
-source = event.Source;
-ind = get(source,'UserData');
-xdata = get(source,'XData')
-ydata = get(source,'YData')
-switch button
-    case 1 %left click set max value
-
-        text(gui.handles.mit_axes,xdata(1),ydata(1) - 10,0,num2str(-(RAMP.mit_peaks(ind)-128)/128));
-
-
-
-end
-
-end
-
-
-function curve_callback(gcbo,event)
-
-global DATA RAMP
-gui = RAMP.gui;
-x = round(event.IntersectionPoint(1,1));
-
-for i=1:length(RAMP.splits)
-    dist(i)= RAMP.splits(i) - x;
-
-end
-dist(dist<0)=nan;
-
-[val, ind] = min(dist);
-ind = ind -1;
-
-
-if x>RAMP.splits(1) && x<RAMP.splits(end)
-    if gcbo == gui.handles.mit_axes
-        RAMP.mit_inds(ind) = x;
-        RAMP.mit_peaks(ind) = 255-RAMP.mit_curve(x);
-        set(gui.mit_peak(ind),'XData',[x x]);
-        set(gui.mit_peak(ind),'YData',[RAMP.mit_peaks(ind) RAMP.mit_peaks(ind)]);
-    end
-
-    if gcbo == gui.handles.tri_axes
-        RAMP.tri_inds(ind) = x;
-        RAMP.tri_peaks(ind) = 255-RAMP.tri_curve(x);
-        set(gui.tri_peak(ind),'XData',[x x]);
-        set(gui.tri_peak(ind),'YData',[RAMP.tri_peaks(ind) RAMP.tri_peaks(ind)]);
-    end
-end
-
-[RAMP.mit_min_val,RAMP.mit_max_val,RAMP.mit_min_ind,RAMP.mit_max_ind]=get_maxmin(RAMP.mit_peaks);
-[RAMP.tri_min_val,RAMP.tri_max_val,RAMP.tri_min_ind,RAMP.tri_max_ind]=get_maxmin(RAMP.tri_peaks);
-
-set([gui.mit_peak(:); gui.tri_peak(:)], 'MarkerSize', 5);
-set([gui.mit_peak(RAMP.mit_max_ind), gui.mit_peak(RAMP.mit_min_ind), gui.tri_peak(RAMP.tri_max_ind), gui.tri_peak(RAMP.tri_min_ind)], 'MarkerSize', 10);
-set([gui.l(1), gui.l(2)], 'XData', [RAMP.mit_inds(RAMP.mit_max_ind), RAMP.mit_inds(RAMP.mit_max_ind)]);
-set([gui.l(3), gui.l(4)], 'XData', [RAMP.tri_inds(RAMP.tri_max_ind), RAMP.tri_inds(RAMP.tri_max_ind)]);
-
-update_table();
-
-end
-
-
-function img = flow_histogram(velocities)
-% Construct a flow histogram or spectral plot where each voxel contributes one intensity value on the y-level of its velocity.
-
-img = zeros([256 size(velocities,1)]);
-for time_index=1:size(velocities,1)
-    for pixel_index=1:length(velocities(time_index,:))
-        % + 1 is needed here because matlab is awful
-        ind = (velocities(time_index,pixel_index))*255+1;
-        ind(ind<0)=0;
-        ind(ind>255)=255;
-        img(round(ind),time_index)=img(round(ind),time_index)+1;
-    end
-end
-end
-
-function img = flow_histogram_no_low(velocities)
-% Construct a flow histogram or spectral plot where each voxel contributes one intensity value on the y-level of its velocity.
-% Exclude -4 to 4 v velocites like in Doppler echo
-
-dist = 4;
-img = zeros([256 size(velocities,1)]);
-for time_index=1:size(velocities,1)
-    for pixel_index=1:length(velocities(time_index,:))
-        % + 1 is needed here because matlab is awful
-
-        ind = (velocities(time_index,pixel_index))*255+1;
-        ind(ind<0)=0;
-        ind(ind>255)=255;
-        if ind >128+dist || ind < 128-dist
-            img(round(ind),time_index)=img(round(ind),time_index)+1;
-        end
-
-    end
-    img(128,time_index)=255;
-end
-
-end
-
-function curve= max_percentile_curve(velocity,percentile)
-%constructs the vt curve from the of voxels with above or equal to the nth
-%percentile in the timeframe
-for i=1:size(velocity,1)
-    value = prctile(velocity(i,:),percentile);
-
-    all_values = velocity(i,:);
-    values_above_percentile = all_values(all_values>=value);
-    %length(values_above_percentile)
-    curve(i) = sum(values_above_percentile)/length(values_above_percentile)*255+1;
-
-end
-end
-
-
-function curve = maxk_curve(velocity,k)
-%constructs the vt curve from the k nr of voxels with the highest velocity
-%in the timeframe
-
-for i=1:size(velocity,1)
-    curve(i)=(sum(maxk(velocity(i,:),k))/k)*255+1;
-end
-end
-
-function [peaks,inds] = find_ewave_peaks(velocity,splits,offset)
-%This function is used to identify the peaks of the ewave.
-ewaves={};
-global RAMP
-gui = RAMP.gui;
-
-for i=1:length(velocity)
-    if velocity(i)>128
-        velocity(i)=velocity(i);
-    else
-        velocity(i)=128;
-    end
-end
-m = mean(velocity)+offset;
-
-
-for i=1:length(velocity)
-    if velocity(i)>m
-        velocity(i)=velocity(i);
-    else
-        velocity(i)=m;
-    end
-end
-for i=1:length(splits)-1
-    ewaves{i}=velocity(splits(i):splits(i+1));
-end
-
-
-
-
-for i=1:size(ewaves,2)
-    %for ii=1:size(ewaves{i})
-    t = splits(i);
-
-    %bias = linspace(1,0.9,length(ewaves{i}));
-    %test = ewaves{i}.*bias;
-
-    [pks,locs,w,h] = findpeaks(ewaves{i});
-
-    if(isempty(pks))
-        peaks(i)=-1
-        inds(i)=t-9;
-    else
-        [maxv,maxind] = max(w);
-        peak_e_ind = locs(maxind);%min(locs);
-        peaks(i) =255-ewaves{i}(peak_e_ind);
-        inds(i) = peak_e_ind+t-1;
-
-    end
-    %end
-end
-
-end
-
-function splits = calculate_negative_flow(velocity,min_peak_distance)
-%this function is used to determine the negative peaks of mitral flow,
-%which corresponds to aortic outflow during systole. This is used to
-%separate each heartbeat
-
-global DATA SET RAMP
-gui=RAMP.gui;
-
-
-neg_velocity = sum(mink(velocity,25,2),2)/25;
-neg_velocity = neg_velocity(neg_velocity<0.5);
-%Plot for debugging
-%gui.test = plot(255-neg_velocity*255,'color','red','LineWidth',1,'Parent',gui.handles.mit_axes,'PickableParts','none');
-B = 1/5*ones(5,1);
-neg_velocity = filter(B,1,neg_velocity);
-[peak,loc]=findpeaks(-neg_velocity,'MinPeakDistance',min_peak_distance);
-splits = loc-3;
-end
-
-function draw_mmode(x)
-global DATA SET RAMP
-gui=RAMP.gui;
-nom = RAMP.nom;
-nop = RAMP.nop;
-
-%gui.phase = imagesc(squeeze(SET(nom).IM(:,x,:)),'Parent',gui.handles.mmd_axes);
-set(gui.mmd,'CData',squeeze(SET(nom).IM(:,x,:)));
-end
-
-function checkboxes_callback(varargin)
-global DATA RAMP
-gui = RAMP.gui;
-switch varargin{1}.Tag
-    case 'checkbox_kmax_line'
-        set([gui.mit_curve gui.tri_curve],'Visible',get(varargin{1},'Value'));
-
-    case 'checkbox_beat_seg'
-        set(gui.split_line(:),'Visible',get(varargin{1},'Value'));
-end
-
-end
-
-function mag_click_callback(gcbo,event)
-%Select what slice to use as mmode
-global DATA RAMP;
-gui = RAMP.gui;
-pos = get(get(gco,'Parent'),'CurrentPoint');
-h = gco;
-
-t = pos(1,1);
-draw_mmode(round(t));
-
-set(RAMP.overview_line,'Xdata',[t t]);
-%uistack(RAMP.overview_line,'top');
-end
-
-function [max_val,min_val,max_ind,min_ind] = get_maxmin(peaks)
-[max_val,max_ind] = max(peaks);
-[min_val,min_ind] = min(peaks);
-end
-
+%Utility funct
 function arraystring = num2clip(array)
 %Copies the result to the clipboard for pasting
 arraystring = num2str(array);
